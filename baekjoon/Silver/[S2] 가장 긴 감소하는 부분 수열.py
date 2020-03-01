12e7e1b7b9ecb79f7b164950e77e59cf638173f5
abcworld123 <rkaxhdals@gmail.com>
2020-03-01 16:49:47 +0900

    [S2] 가장 긴 감소하는 부분 수열

---
+++ baekjoon/Silver/[S2] 가장 긴 감소하는 부분 수열.py
@@ -0,0 +1,9 @@
+input()
+arr = list(map(int, input().split()))
+
+for i in range(len(arr)):
+	arr[i] = [arr[i], 0]
+	less = [x for x in arr[:i] if x[0] > arr[i][0]]
+	arr[i][1] = max(less, key=lambda x: x[1])[1] + 1 if len(less) else 1
+
+print(max(arr, key=lambda x: x[1])[1])
