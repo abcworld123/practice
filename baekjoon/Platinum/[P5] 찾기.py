ce99cc4d6b7ee9ef4a1580cb21a9c0e4d8c01ad1
abcworld123 <rkaxhdals@gmail.com>
2021-11-01 20:10:19 +0900

    [P5] 찾기

--- baekjoon/Platinum/[P5] 찾기.py
+++ baekjoon/Platinum/[P5] 찾기.py
@@ -1,4 +1,3 @@
-# kmp
 def makeTable(s):
 	table = [0] * len(s)
 	j = 0
@@ -13,26 +12,23 @@ def makeTable(s):
 
 def search(table, a, b):
 	i, j = 0, 0
+	arr = []
 	while i < len(a):
 		if a[i] == b[j]:
 			i += 1
 			j += 1
-			if j == len(b): return 1
+			if j == len(b):
+				arr.append(i - len(b) + 1)
+				j = table[j]
 		else:
 			if j == 0: i -= table[j]
 			else: j = table[j]
-	return 0
+	return arr
 
 stdin = [*open(0)]
 a = stdin[0]
 b = stdin[1].replace('\n', '')
 table = makeTable(b)
-ans = search(table, a, b)
-print(ans)
-
-
-
-# 숏코딩
-import re
-a=input()
-print(1if re.search(input(),a)else 0)
+arr = search(table, a, b)
+print(len(arr))
+for x in arr: print(x)
