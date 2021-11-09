3bf66c02881935fb3a7f2bcea7ef69d6a725ea0a
abcworld123 <rkaxhdals@gmail.com>
2021-11-10 02:56:44 +0900

    [G2] 홀수/짝수번째 피보나치 수의 합

---
+++ baekjoon/Gold/[G2] 짝수번째 피보나치 수의 합.py
@@ -0,0 +1,11 @@
+def f(fibo, n, m):
+    if n < 3: return fibo[n] % m
+    half, odd = n >> 1, n & 1
+    if half not in fibo: fibo[half] = f(fibo, half, m)
+    if half + 1 not in fibo: fibo[half + 1] = f(fibo, half + 1, m)
+    if not odd and half - 1 not in fibo: fibo[half - 1] = f(fibo, half - 1, m)
+    return (fibo[half + odd] * fibo[half + 1] + fibo[half - 1 + odd] * fibo[half]) % m
+
+n = int(input())
+fibo = {0: 0, 1: 1, 2: 1}
+print(f(fibo, n + (n + 1 & 1), 1000000007) - 1)
