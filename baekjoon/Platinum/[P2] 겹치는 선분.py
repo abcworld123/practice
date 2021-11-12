07810f16f3ae68c72e77fa8ce92c363b188e998c
abcworld123 <rkaxhdals@gmail.com>
2021-11-12 22:43:20 +0900

    [P2] 겹치는 선분

---
+++ baekjoon/Platinum/[P2] 겹치는 선분.py
@@ -0,0 +1,31 @@
+import sys
+from fractions import Fraction
+from collections import defaultdict
+input = sys.stdin.readline
+
+ans = 0
+lines = defaultdict(list)
+
+for _ in range(int(input())):
+    x1, y1, x2, y2 = map(int, input().split())
+    if x1 == x2:
+        key = f'inf {x1}'
+        if y1 <= y2: x1, x2 = y1, y2
+        else: x1, x2 = y2, y1
+    else:
+        a = Fraction(y2 - y1, x2 - x1)
+        y0 = y1 - (a * x1)
+        key = f'{a} {y0}'
+        if x1 > x2: x1, x2 = x2, x1
+    lines[key].append((x1, 1))
+    lines[key].append((x2, -1))
+
+for arr in lines.values():
+    if len(arr) == 2: continue
+    arr.sort()
+    cur = 0
+    for p in arr:
+        if p[1] == 1: ans += cur
+        cur += p[1]
+
+print(ans)
