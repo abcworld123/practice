e530ae1218ccadc1714aebc493fd157fdb1e70cd
abcworld123 <rkaxhdals@gmail.com>
2021-11-15 22:58:13 +0900

    [P5] 순열

---
+++ baekjoon/Platinum/[P5] 순열.py
@@ -0,0 +1,22 @@
+import sys
+input = sys.stdin.readline
+
+def init(seg, s, e, i):
+    if s == e: seg[i] = 1
+    else: seg[i] = init(seg, s, (s + e) >> 1, i << 1) + init(seg, ((s + e) >> 1) + 1, e, (i << 1) + 1)
+    return seg[i]
+
+def get(seg, s, e, i, x):
+    while s != e:
+        seg[i] -= 1
+        if x <= seg[i << 1]: e = (s + e) >> 1; i = i << 1
+        else: s = ((s + e) >> 1) + 1; x -= seg[i << 1]; i = (i << 1) + 1
+    seg[i] -= 1
+    return s
+
+
+N = int(input())
+seg = [-1] * (1 << (N.bit_length() + 1))
+init(seg, 1, N, 1)
+ans = [get(seg, 1, N, 1, int(input()) + 1) for _ in range(N)]
+print(*[N - k[0] for k in sorted(enumerate(ans[::-1]), key=lambda x: x[1])])
