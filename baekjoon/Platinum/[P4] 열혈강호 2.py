a3eb416ea3eab971bb624d015b63ede12a0d178b
abcworld123 <rkaxhdals@gmail.com>
2021-12-13 05:10:25 +0900

    [P4] 열혈강호 2

---
+++ baekjoon/Platinum/[P4] 열혈강호 2.py
@@ -0,0 +1,31 @@
+import sys
+input = sys.stdin.readline
+
+def dfs(visited, i):
+    visited[i] = True
+    for v in graph[i]:
+        if R[v] == -1:
+            R[v] = i
+            return True
+    for v in graph[i]:
+        if not visited[R[v]] and R[v] != i and dfs(visited, R[v]):
+            R[v] = i
+            return True
+    return False
+
+
+N, M = map(int, input().split())
+graph = []
+R = [-1] * M
+ans = 0
+
+for i in range(N):
+    _, *want = list(map(int, input().split()))
+    graph.append([v - 1 for v in want])
+
+for i in range(N):
+    if dfs([False] * N, i): ans += 1
+    if dfs([False] * N, i): ans += 1
+    if ans == M: break
+
+print(ans)
