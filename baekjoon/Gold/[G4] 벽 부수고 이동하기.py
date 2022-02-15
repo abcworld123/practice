4165852ced64a0530112532de7671f54d54d9036
abcworld123 <rkaxhdals@gmail.com>
2022-02-16 02:16:21 +0900

    [G4] 벽 부수고 이동하기

---
+++ baekjoon/Gold/[G4] 벽 부수고 이동하기.py
@@ -0,0 +1,25 @@
+import sys
+from collections import deque
+input = sys.stdin.readline
+
+N, M = map(int, input().split())
+arr = [[2] * (M + 2)] + [[2] + list(map(int, input().rstrip())) + [2] for _ in range(N)] + [[2] * (M + 2)]
+visited = [[[False] * (M + 2) for _ in range(N + 2)] for _ in range(2)]
+d = [(-1, 0), (0, 1), (1, 0), (0, -1)]
+q = deque([(1, 1, 1, 0)])
+
+while q:
+    i, j, m, b = q.popleft()
+    if i == N and j == M: print(m); exit()
+    for dy, dx in d:
+        y, x = i + dy, j + dx
+        if arr[y][x] == 0:
+            if not visited[b][y][x]:
+                visited[b][y][x] = True
+                q.append((y, x, m + 1, b))
+        elif arr[y][x] == 1 and b == 0:
+            if not visited[b][y][x]:
+                visited[b][y][x] = True
+                q.append((y, x, m + 1, 1))
+
+print(-1)
